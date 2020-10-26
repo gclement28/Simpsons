92461fedd218e6a1ff84716ca03620285595d2dd
Clement Garcin <69050287+gclement28@users.noreply.github.com>
2020-10-26 22:06:54 +0100

    added new quotes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Simpsons from './components/SimpsonsQuotes';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const simpsonsQuote = [{"quote":"Why are you pleople avoiding me? Does my withered face remind you of the grim specter of death?","character":"Abe Simpson","image":"https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FAbrahamSimpson.png?1497567511593","characterDirection":"Right"}]
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      simpsons: simpsonsQuote,
+    };
+    this.getSimpsons = this.getSimpsons.bind(this);
+  }
+
+  getSimpsons() {
+    // Send the request
+    axios
+      .get('https://thesimpsonsquoteapi.glitch.me/quotes')
+      // Extract the DATA from the received response
+      .then((response) => response.data)
+      // Use this data to update the state
+      .then((data) => {
+        this.setState({
+          simpsons: data,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <Simpsons simpsons={this.state.simpsons} />
+
+        <button type='button' onClick={this.getSimpsons}>
+          Refresh
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
